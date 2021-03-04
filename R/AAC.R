2efb833a98553cd7219442477deab567573eba7a
Laura <erdaradun.gaztea@gmail.com>
2021-03-04 22:27:52 +0100

    removed unused import

--- R/AAC.R
+++ R/AAC.R
@@ -25,7 +25,6 @@ AAC <- function(x, ...)
 AAC.default <- function(x, ...)
   stop("'AAC' isn't implemented for this type of object", call. = FALSE)
 
-#' @importFrom dplyr bind_rows
 #' @rdname AAC
 #' @export
 AAC.sq_ami_bsc <- function(x, ...,
