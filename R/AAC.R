af0567148d6e948a42e901d4387025bed1054b18
Laura <erdaradun.gaztea@gmail.com>
2021-03-04 22:23:22 +0100

    added docs for AAC

--- R/AAC.R
+++ R/AAC.R
@@ -1,3 +1,22 @@
+#' Compute AAC matrix
+#'
+#' @description Computes AAC (Amino Acid Composition) matrix, which contains
+#' frequencies of each of 20 basic amino acids for each sequence.
+#'
+#' @param x [\code{sq_ami_bsc}]\cr
+#'  An object this function is applied to.
+#' @template three-dots
+#' @template NA_letter
+#'
+#' @details
+#' Frequency of an amino acid is understood as the number of times this amino
+#' acid appears in the sequence divided by total number of elements of this
+#' sequence.
+#'
+#' @examples
+#' sq_ami <- tidysq::sq(c("PPAVMMFDILKKIQ", "PQEWYTWLPVMCTN"))
+#' AAC(sq_ami)
+#'
 #' @export
 AAC <- function(x, ...)
   UseMethod("AAC")
@@ -9,6 +28,7 @@ AAC.default <- function(x, ...)
 #' @importFrom dplyr bind_rows
 #' @rdname AAC
 #' @export
-AAC.sq_ami_bsc <- function(x, ...) {
-  CPP_AAC(x, getOption("tidysq_NA_letter"))
+AAC.sq_ami_bsc <- function(x, ...,
+                           NA_letter = getOption("tidysq_NA_letter")) {
+  CPP_AAC(x, NA_letter)
 }
