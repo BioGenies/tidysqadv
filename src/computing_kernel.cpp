5271d1725daff09658c3f5be50e5fa85749ed9f5
Laura <erdaradun.gaztea@gmail.com>
2021-05-29 23:35:36 +0200

    implemented Rcpp-specific correlation kernel, but without template yet

--- src/computing_kernel.cpp
+++ src/computing_kernel.cpp
@@ -10,11 +10,5 @@ Rcpp::NumericMatrix CPP_computing_kernel(const Rcpp::List& x,
                                          const double exponential,
                                          const std::basic_string<char>& NA_letter) {
     Sq<RCPP_IT> sq = import_sq_from_R(x, NA_letter);
-    std::vector<std::vector<double>> result = correlation_kernel_3(sq, max_kmer_length, exponential);
-    std::vector<double> result_flattened = std::accumulate(result.begin(), result.end(), std::vector<double>(),
-            [](std::vector<double> column_1, std::vector<double> column_2) {
-        column_1.insert(column_1.end(), column_2.begin(), column_2.end());
-        return column_1;
-    });
-    return Rcpp::NumericMatrix(sq.size(), sq.size(), result_flattened.cbegin());
+    return correlation_kernel_3_Rcpp(sq, max_kmer_length, exponential);
 }
